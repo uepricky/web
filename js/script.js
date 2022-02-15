ee6fcdf712d8ec6c583f7fcb2acd5ed2a01c502a
kei-6979213 <keisuke.6923@gmail.com>
2022-02-15 18:33:49 +0900

    レスポンシブ、top.css完了

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//ハンバーガーボタン
+$(function () {
+    $('.menu-trigger').click(function () {          //ハンバーガーボタン(.menu-trigger)をタップすると、
+        $(this).toggleClass('active');　　　　　　　 //タップしたハンバーガーボタン(.menutrigger)に(.active)を追加・削除する。
+        if ($(this).hasClass('active')) {　　　　　　//もし、ハンバーガーボタン(.menu-trigger)に(.active)があれば、
+          $('.g-navi').addClass('active');　　　　　 //(.g-navi)にも(.active)を追加する。
+        }　　　　　　　　　　　　　　　　　　　　　　　//それ以外の場合は、
+        else{                                        //(.g-navi)にある(.active)を削除する。
+            $('.g-navi').removeClass('active');
+        }
+    });
+    $('.nav-wrapper ul li a').click(function (){     //各メニューリンク(.nav-wrapper ul li a)タップすると、
+        $('.menu-trigger').removeClass('active');    //ハンバーガーボタン(.menu-trigger)みある(.active)を削除する。
+        $('.g-navi').removeClass('active');          //(.g-navi)にある(.active)も削除する。
+    });
+});
