692f84fe2b962569f2c8a38a821ab5a92b7bb1a4
kei-6979213 <keisuke.6923@gmail.com>
2022-03-02 14:49:00 +0900

    jqueryで所々にに動きをつけてみました。

--- common/js/script.js
+++ common/js/script.js
@@ -15,7 +15,7 @@ $(function () {
     });
 });
 
-
+/*header スクロール変化*/
 jQuery(window).on('scroll', function () {
  
     if (jQuery('.header').height() < jQuery(this).scrollTop() > 0) { 
@@ -24,10 +24,35 @@ jQuery(window).on('scroll', function () {
     jQuery('.header').removeClass('header_active'); } });
     
     
-
+/*header スクロール変化、レスポンシブ*/
 jQuery(window).on('scroll', function () {
  
     if (jQuery('.g-navi-sp').height() < jQuery(this).scrollTop() > 0) { 
     jQuery('.g-navi-sp').addClass('active'); }
     else {
-    jQuery('.g-navi-sp').removeClass('active'); } });  
+    jQuery('.g-navi-sp').removeClass('active'); } }); 
+
+
+/*top-service画像(フェードイン)*/
+$(function(){
+    $(".inview_re").on("inview", function (event, isInView) {
+      if (isInView) {
+        $(this).stop().addClass("is-show");
+      } else {
+        $(this).stop().removeClass("is-show");
+      }
+    });
+  });
+  
+/*company本文(フェードイン)*/
+  $('#animation').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('#animation').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 400){
+   $(this).addClass("fadeInDown");
+  }
+ });
+});
